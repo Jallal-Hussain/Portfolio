4282545b59114f47ebf6d6d6b1589a75c51847c2
Jalal Hussain <jalal143880@gmail.com>
2025-07-08 11:17:36 +0500

    Updated experiences.

--- src/data/experienceData.jsx
+++ src/data/experienceData.jsx
@@ -1,30 +1,30 @@
 export const experienceData = [
-  {
-    id: 1,
-    company: "NetBots",
-    jobtitle: "Frontend Developer Intern",
-    startYear: "2024",
-    endYear: "Present",
-  },
+  // {
+  //   id: 4,
+  //   company: "NetBots",
+  //   jobtitle: "Frontend Developer Intern",
+  //   startYear: "2024",
+  //   endYear: "Present",
+  // },
+  // {
+  //   id: 3,
+  //   company: "NetBots",
+  //   jobtitle: "Backend Developer Intern",
+  //   startYear: "2024",
+  //   endYear: "Present",
+  // },
   {
     id: 2,
     company: "NetBots",
-    jobtitle: "Backend Developer Intern",
-    startYear: "2024",
-    endYear: "Present",
-  },
-  {
-    id: 3,
-    company: "NetBots",
     jobtitle: "React Developer Intern",
-    startYear: "2024",
-    endYear: "Present",
+    startYear: "September 2024",
+    endYear: "March 2025",
   },
   {
-    id: 4,
+    id: 1,
     company: "NetBots",
     jobtitle: "MERN Stack Developer Intern",
-    startYear: "July 2024",
+    startYear: "April 2025",
     endYear: "Present",
   },
 ];
